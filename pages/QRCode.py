6774cecdd818550b7132bcfa5d1e2e1d94be70eb
Robin Kraemer <113530153+Sendarr67@users.noreply.github.com>
2022-11-20 19:12:21 +0100

    Add files via upload

---
+++ pages/QRCode.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(page_title = 'Movie recommandation system',
+	               page_icon = 'Logo.png',layout = "centered"  )
+
+
+st.image("unitag_qrcode_standard.png", use_column_width=True)
