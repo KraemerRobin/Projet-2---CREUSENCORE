535e0a468e7c4cf6d14f67ee4abd8c1df492233a
Robin Kraemer <113530153+Sendarr67@users.noreply.github.com>
2022-11-21 11:16:27 +0100

    Update CREUSENCORE.py

--- CREUSENCORE.py
+++ CREUSENCORE.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 from bokeh.models.widgets import Div
 
+
 st.set_page_config(page_title = 'Movie recommendation system',
 	               page_icon = 'Logo.png',layout = "centered",initial_sidebar_state = "collapsed"  )
 def add_bg_from_local(image_file):
@@ -36,19 +37,18 @@ df_ML =  pd.read_pickle('df_main.zip')
 movie  = st.multiselect("", df_ML, max_selections=1)
 
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 
 with col1 : 
-    weight_genre = st.slider('Genre', min_value=1, max_value=100, step=1, key='test')
+    weight_year = st.slider('Year', min_value=1, max_value=10, step=1, key='test2')
     
 with col2 : 
-    weight_actor = st.slider('Actors', min_value=1, max_value=100, step=1, key='test4')
+    weight_actor = st.slider('Actors', min_value=1, max_value=50, step=1, key='test4')
     
 with col3 : 	
-    weight_director = st.slider('Director', min_value=1, max_value=100, step=1, key='test44')
+    weight_director = st.slider('Director', min_value=1, max_value=10, step=1, key='test44')
 
-with col4 : 
-    weight_year = st.slider('Year', min_value=1, max_value=100, step=1, key='test2')
+
 
 
 
@@ -69,7 +69,7 @@ if button2 :
             X_scaled[:, 0] = X_scaled[:, 0]*(weight_year)
             X_scaled[:, 2] = X_scaled[:, 2]*60
             X_scaled[:, 3] = X_scaled[:, 3]*16
-            X_scaled[:, 4:28] = X_scaled[:, 4:28]*(weight_genre)*74
+            X_scaled[:, 4:28] = X_scaled[:, 4:28]*74
             X_scaled[:, 28:163] = X_scaled[:, 28:163]
             X_scaled[:, 163:1589] = X_scaled[:, 163:1589]*(weight_actor)
             X_scaled[:, 1589:] = X_scaled[:, 1589:]*(weight_director)*3
